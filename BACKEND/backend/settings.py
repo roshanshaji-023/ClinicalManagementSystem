1c0978394b50aa573bcbf5092dd7362804c3288f
batman <thezezfieofficial@gmail.com>
2026-02-27 23:12:35 +0530

    renamed app administraton to administration and updated settings.py

--- BACKEND/backend/settings.py
+++ BACKEND/backend/settings.py
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication',
+    'administration',
+    'doctor',
+    'labtechnician',
+    'pharmacist',
+    'reception',
 ]
 
 MIDDLEWARE = [
