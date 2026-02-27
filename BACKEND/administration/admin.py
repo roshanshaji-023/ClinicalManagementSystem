643f93cacbd2b1ed79e0fd90406b3f8d6097bbba
batman <thezezfieofficial@gmail.com>
2026-02-28 00:05:22 +0530

    added models to admin.py

--- BACKEND/administration/admin.py
+++ BACKEND/administration/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Department,Staff,Doctor
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Doctor)
+admin.site.register(Staff)
