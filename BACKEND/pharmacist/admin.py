d7c1bf585918f1507f3ad5d3cbe45e8c9284f77f
siva <sivabharathsivabharath04@gmail.com>
2026-03-02 15:56:12 +0530

    Fix imports and update pharmacist models & serializers

--- BACKEND/pharmacist/admin.py
+++ BACKEND/pharmacist/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import MedicineInventory,MedicinePurchaseHistory,MedicineType
+from .models import MedicineInventory,MedicinePurchaseHistory,MedicineType,MedicinePrescription
 
 # Register your models here.
 admin.site.register(MedicineInventory)
 admin.site.register(MedicinePurchaseHistory)
 admin.site.register(MedicineType)
+admin.site.register(MedicinePrescription)
