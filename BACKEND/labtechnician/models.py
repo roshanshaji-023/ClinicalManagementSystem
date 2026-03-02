c7fb9d14008c1c5b75ffc28b466e597b62b4b7f6
AncyNeenu <ancyneenu11@gmail.com>
2026-03-02 20:03:59 +0530

    model.py is updated

--- BACKEND/labtechnician/models.py
+++ BACKEND/labtechnician/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from reception.models import Patient
+from django.core.exceptions import ValidationError
+from administration.models import Doctor
+from reception.models import Appointment
 
 # Create your models here.
 
@@ -10,5 +12,34 @@ class LabTestType(models.Model):
 
     def __str__(self):
         return self.Lab_test_name
+    
 
+class LabTestPrescription(models.Model):
+    lab_hist_id = models.AutoField(primary_key=True) 
+    appointment = models.ForeignKey(Appointment,on_delete=models.CASCADE,related_name='lab_tests')
+    doctor= models.ForeignKey(Doctor,on_delete=models.CASCADE,related_name='lab_tests')
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Completed', 'Completed'),
+        ]
+    test_type = models.ForeignKey(LabTestType,on_delete=models.CASCADE,related_name='prescriptions')
+    status =models.CharField(max_length=20,choices=STATUS_CHOICES,default='Pending')
+    result = models.TextField(blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ('appointment', 'test_type')  # prevents duplicate test for same appointment
+    
+    #model-level validation
+    def clean(self):
+        """
+        Ensure doctor matches appointment doctor
+        """
+        if self.appointment.doctor != self.doctor_id:
+            raise ValidationError(
+                "Doctor must match the appointment doctor."
+            )
 
+    def __str__(self):
+        return f"{self.appointment.patient} - {self.test_type.Lab_test_name} ({self.status})"
