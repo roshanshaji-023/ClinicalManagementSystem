81a9adbcec7ac7e25ec0b1a3270dc01136e35c3d
AncyNeenu <ancyneenu11@gmail.com>
2026-03-02 12:32:53 +0530

    added labtesttype in models.py

--- BACKEND/labtechnician/models.py
+++ BACKEND/labtechnician/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Empty:
-    pass
+class LabTestType(models.Model):
+    Lab_test_id=models.AutoField(primary_key=True)
+    Lab_test_name = models.CharField(max_length=255)
+    Lab_test_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.Lab_test_name
+
