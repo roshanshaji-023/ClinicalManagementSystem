c5fb953c4448ca8fe1b6babbdd001d719a0f07ea
Roshan Shaji <54848410+roshanshaji-023@users.noreply.github.com>
2026-03-04 10:20:22 +0530

    Leadtask (#11)
    * completed serializer,viewsand urls
    
    * modified urls.py and serializer.py
    
    * modified serializer.py
    
    * modified serializer.py
    
    * modified serializer.py
    
    * modofied setings
    
    ---------
    
    Co-authored-by: batman <thezezfieofficial@gmail.com>

--- BACKEND/backend/settings.py
+++ BACKEND/backend/settings.py
@@ -132,12 +132,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#                'rest_framework.authentication.TokenAuthentication',
-#     ),
-#     'DEFAULT_PERMISSION_CLASSES':(
-#                 'rest_framework.permissions.IsAuthenticated',
-#     ),
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.AllowAny',
+    ),
+
 
 # }
